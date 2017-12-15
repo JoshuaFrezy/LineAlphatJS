859747e13093de8d74887c6cddf0e930735691ba
JoshuaFrezy <32793563+JoshuaFrezy@users.noreply.github.com>
2017-12-14 19:32:02 -0800

    Update bot.js

--- src/bot.js
+++ src/bot.js
@@ -1,10 +1,18 @@
 const LineConnect = require('./connect');
-let line = require('./main.js');
-let LINE = new line();
+let LINE = require('./main.js');
+console.info("\n\
+=========================================\n\
+BotName: LINE Etot JS\n\
+Version: 0.2.1\n\
+Terima Kasih Kepada @Alfathdirk @TCR_TEAM\n\
+=========================================\n\
+\nNOTE : Ini Adalah AlphatJS Lama Buatan @Alfathdirk @TCR_TEAM Dan Ini Telah Dikembangin Oleh @TAB_TEAM\nTolong Untuk Tidak Perjual-Belikan Script Ini!\n\
+****Nekopoi.host Running****");
 
-// const auth = {
-// 	authToken: ' Token Here',
-// }
+const auth = {
+	authToken: ' Toket Taro Sini Eh Token Maksudnya ',
+	certificate: ' Taro Cerf Disini ',
+}
 // let client =  new LineConnect(auth);
 let client =  new LineConnect();
 
